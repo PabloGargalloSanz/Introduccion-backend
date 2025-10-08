622564e6c8611bbef345c1b26c05d20246072d56
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-10-08 13:06:13 +0200

    Feat: simulacion db

---
+++ 02-estructura/db/momy.js
@@ -0,0 +1,35 @@
+
+export const users = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email:'johndeep@gmail.com',
+        password: '123',
+        role: 'admin'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email:'janeDoe@gmail.com',
+        password: '1234',
+        role: 'user'
+    }
+
+];
+
+export const alumnos = [
+    {
+        id: 1,
+        name: 'Alumno 1',
+        age: 20,
+        cours: 'Math'
+
+    },
+    {
+        id: 2,
+        name: 'Alumno 2',
+        age: 25,
+        cours: 'Science'
+
+    },
+];
