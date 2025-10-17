fb237dbfdfea34b354225cae5cd4d11a81e41e9f
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-10-17 13:52:48 +0200

    Feat: post Cliente

--- 03-crud/services/clientes.service.js
+++ 03-crud/services/clientes.service.js
@@ -5,3 +5,15 @@ export const getAllClientes = async () => {
     console.log(result.rows);
     return result.rows;
 }
+
+export const newClienteService = async (data) => {
+    // descomponer un json en variables
+    const { nombre, telefono, ubicacion } = data;
+
+    const result = await pool.query(
+        'INSERT INTO clientes (nombre, telefono, ubicacion) VALUES ($1, $2, $3) RETURNING *',
+        [nombre, telefono, ubicacion]
+        //returning devuelve toda la fila insertada
+    );
+    return result.rows[0];
+}
