dd4055185a062be99b358a4448381344f40016d2
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-10-22 15:55:14 +0200

    Feat: Crear archivo log cada dia

--- 03-crud/middlewares/logger.js
+++ 03-crud/middlewares/logger.js
@@ -2,13 +2,19 @@ import fs from 'fs';
 import path from 'path';
 
 const logger = (req, res, next) => {
-    const string = `[${new Date().toISOString()}] ${req.method} - ${req.url} - ${req.ip}`
+    //coge solamente la parte antes de la T, lo de detras es la hora
+    const fecha = new Date().toISOString().split('T')[0];
+    const string = `[${new Date().toISOString()}] ${req.method} - ${req.url} - ${req.ip}`;
+    
+    //proceso actual, directorio y nombre carpeta
+    const direccion = path.join(process.cwd(), 'logs');
+    const nombreArchivo = path.join(direccion, fecha +'.log');
 
     console.log(string);
+    console.log(fecha);
 
-    fs.appendFile('./logs/request.log',
-        string + '\n',
-        (error) => {
+    fs.appendFile(nombreArchivo, string + '\n', 
+            (error) => {
             if(error) {
                 console.log( error);
             }
